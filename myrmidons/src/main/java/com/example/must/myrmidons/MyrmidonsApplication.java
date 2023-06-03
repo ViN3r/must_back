68b867c2b822a8cab35b561a6e91f2853670b674
Ervin <ervin.milandou@gmail.com>
2023-06-03 22:01:44 +0200

    Initialisation projet back

---
+++ myrmidons/src/main/java/com/example/must/myrmidons/MyrmidonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.must.myrmidons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrmidonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrmidonsApplication.class, args);
+	}
+
+}
